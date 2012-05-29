29ec5d89b2f692d518f18ddc33ace26f47350030
Andrew Goodwin <andrewggoodwin@gmail.com>
2012-05-29 14:44:43 -0500

    changed to 6 from laptop

--- LobbyKiosk/LobbyKiosk/AppDelegate.m
+++ LobbyKiosk/LobbyKiosk/AppDelegate.m
@@ -25,7 +25,7 @@
     MasterViewController *masterViewController = [[MasterViewController alloc] initWithNibName:@"MasterViewController" bundle:nil];
     UINavigationController *masterNavigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
     
-    NSLog(@"Change 5");
+    NSLog(@"Change 6");
 
     DetailViewController *detailViewController = [[DetailViewController alloc] initWithNibName:@"DetailViewController" bundle:nil];
     UINavigationController *detailNavigationController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
